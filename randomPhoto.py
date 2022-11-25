8094622bbc3dcbc581651d1c5a816646ef97e1c2
icrescenti <icrescenti@terkstudios.com>
2022-11-25 08:09:48 +0100

    cleanup

--- randomPhoto.py
+++ randomPhoto.py
@@ -9,6 +9,8 @@ minutes = [0,0,0,0,0]
 
 for i in range(_max):
     randHour = random.randint(8, 16)
+    
+    #exclude 14:00
     if (randHour == 14):
         randHour += 1
 
@@ -17,17 +19,9 @@ for i in range(_max):
 
 hours.sort()
 
-#debug
-#hours[4] = 8
-#minutes[4] = 12
-
-#print(hours, minutes)
-
 while True:
     now = datetime.datetime.now()
-    #print("-------------------")
     for i in range(_max):
-        #print(hours[i], minutes[i], "==", now.hour, now.minute, (hours[i] == now.hour and minutes[i] == now.minute))
         if (hours[i] == now.hour and minutes[i] == now.minute):
-            os.system('fswebcam -r 1280x720 --no-timestamp --no-underlay --no-banner ./' + str(now.year) + "-" + str(now.month) + "-" + str(now.day) + "-" + str(now.hour) + str(now.minute) + '00.jpg')
+            os.system('fswebcam -r 1280x720 --no-timestamp --no-underlay --no-banner ./' + str(now.year) + "-" + str(now.month) + "-" + str(now.day) + "-" + str(now.hour) + "_" + str(now.minute) + '_00.jpg')
     time.sleep(30)
