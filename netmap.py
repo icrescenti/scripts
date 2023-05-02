6610291b966878a60a47cf87699d88d0b181328c
icrescenti <icrescenti77@gmail.com>
2023-05-02 11:47:03 +0200

    ask for ip prefix

--- netmap.py
+++ netmap.py
@@ -7,7 +7,7 @@ def ping(host):
     return subprocess.call(command, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT) == 0
 
 # Your subnet
-subnet = '192.168.0.'
+subnet = input('IP prefix: ')# '192.168.0.'
 for ip in range(1, 256):
     if(ping(subnet + str(ip))):
         print(subnet + str(ip))
