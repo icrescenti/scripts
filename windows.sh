18fbb87af68f256dde9692607b1731efe3fa9325
icrescenti <icrescenti77@gmail.com>
2023-05-02 11:54:45 +0200

    purge

--- windows.sh
+++
@@ -1,4 +0,0 @@
-#sudo efibootmgr -n NUMERO_BOOT
-sudo sed -i '/#HibernateMode=reboot/c\HibernateMode=reboot' /etc/systemd/sleep.conf
-systemctl hibernate
-sudo sed -i '/HibernateMode=reboot/c\#HibernateMode=reboot' /etc/systemd/sleep.conf
